fca478bba85a8b74f456144c674654d8952a114b
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-04 01:49:49 +0000

    Add the cats controller to the server.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, function () {
+  const port = 3000;
+  await app.listen(port, function () {
     console.log('App is running on port 3000');
   });
 }
