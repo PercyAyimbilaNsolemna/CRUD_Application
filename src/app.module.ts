c138b023ee4729baf24b4262d9bb1e03e49c41d1
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-06 22:15:25 +0000

    Add a controller for sheep.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { DogsController } from './dogs/dogs.controller';
+import { SheepController } from './sheep/sheep.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, DogsController],
+  controllers: [AppController, CatsController, DogsController, SheepController],
   providers: [AppService],
 })
 export class AppModule {}
