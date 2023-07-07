d339aeecd28bec25626f8d74c931df8c613bbba5
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-07 18:18:49 +0000

    Add a get and post route for the animals route.

--- src/animals/animals.controller.ts
+++ src/animals/animals.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('animals')
-export class AnimalsController {}
+export class AnimalsController {
+  //Adds a get route for the root of the animals route
+  @Get('/')
+  getHome(): string {
+    return 'This action returns all the animals';
+  }
+
+  //Adds a POST route for the root fo the animals
+  @Post('/')
+  postHome(): string {
+    return 'This action adds animal(s) to the root route';
+  }
+}
