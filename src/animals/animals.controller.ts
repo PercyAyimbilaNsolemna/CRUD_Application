fe3bb5bd70b042f1ba46f0faa555a02797e862c8
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-07 18:24:07 +0000

    Add a get and post route for the black sub-route in the animals controller.

--- src/animals/animals.controller.ts
+++ src/animals/animals.controller.ts
@@ -13,4 +13,16 @@ export class AnimalsController {
   postHome(): string {
     return 'This action adds animal(s) to the root route';
   }
+
+  //Adds a GET black sub-route to the animals route
+  @Get('/black')
+  getBlack(): string {
+    return 'This action returns all black animals';
+  }
+
+  //Adds a POST black sub-route to the animals route
+  @Post('/black')
+  postBlack(): string {
+    return 'This action adds black animal(s) to the black animals sub-route';
+  }
 }
