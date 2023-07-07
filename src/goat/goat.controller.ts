ab0a1f1c3e7f0e450fa0acf28c02c2b09edec1b8
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-07 19:15:44 +0000

    Add a param to the goat route.

--- src/goat/goat.controller.ts
+++ src/goat/goat.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('goat')
-export class GoatController {}
+export class GoatController {
+  //Adds a root route for the goat controller
+  @Get('/')
+  getHome(): string {
+    return 'This action returns the root route of the goat controller';
+  }
+
+  //Get route for any param on the goat root route
+  @Get(':id')
+  getParam(@Param() params: any): string {
+    console.log(params);
+    return `This action returns the ${params.id} sub-route in the goat controller`;
+  }
+}
